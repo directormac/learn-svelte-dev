66f57656a18075535f9b66c20a3cefefd1368366
directormac <markasena@gmail.com>
2023-06-21 20:51:15 +0800

    buttonclick

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
+	import ButtonClick from './ButtonClick.svelte';
+	import Nested from './Nested.svelte';
 	let name = 'meme';
 	let src = 'meme.jpeg';
 </script>
 
 <h1>Hello {name.toUpperCase()}</h1>
+
+<p>This is a paragraph!</p>
+
+<Nested />
+
+<ButtonClick />
+<br />
 <img {src} alt="a {name} appears" height="600px" width="300px" />
+
+<style>
+	p {
+		color: #ffaa11;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+
+	img {
+		padding-top: 10px;
+	}
+</style>
---
+++ src/routes/Nested.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	let string = `this string contains some <strong> words </strong>`;
+</script>
+
+<p>From nested</p>
+<p>{@html string}</p>
