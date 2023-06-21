a361a4bdaa3f38e3ea7f8219ae225ada4b0e2206
directormac <markasena@gmail.com>
2023-06-21 20:05:19 +0800

    img source

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	let name = 'Svelte';
+	let name = 'meme';
+	let src = 'meme.jpeg';
 </script>
 
 <h1>Hello {name.toUpperCase()}</h1>
+<img {src} alt="a {name} appears" height="600px" width="300px" />
